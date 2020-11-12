0558f7ffac0a34d423e48766a0f0b65ceff16079
paulo.vieira1 <paulo.vieira@acct.global>
2020-11-11 22:41:43 -0300

    primeiro projeto em typescript

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { request, response } from 'express';
+
+
+const app =express();
+
+app.get('/',(request,response)=>{
+    return response.json({message:'Hello word'})
+})
+
+app.listen(3333,()=>{
+    console.log('Server started on port 3333');
+});
+
